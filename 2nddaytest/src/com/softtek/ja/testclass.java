c3740a7e8ad14445bb90e33185af0b5a44d61137
shivan <sgoudaDevelops>
2022-11-07 15:29:46 +0530

    create main class

---
+++ 2nddaytest/src/com/softtek/ja/testclass.java
@@ -0,0 +1,10 @@
+package com.softtek.ja;
+
+public class testclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("hii shivanagoud");
+	}
+
+}
